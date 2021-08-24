2c3ed65416bb5623f7406cea3f9b621d7e33f1f1
김현지 <guswlhjk@naver.com>
2021-08-25 01:51:57 +0900

    Tedpemission 설치 / 권한별 대응 방침설정

--- app/src/main/java/com/nepplus/librarypractice_20210824/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20210824/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.Permissions
 
 class MainActivity : BaseActivity() {
 
@@ -18,10 +21,31 @@ class MainActivity : BaseActivity() {
     override fun setupEvents() {
         callBtn.setOnClickListener {
 
-            //전화 연결 활용
-            val myUri = Uri.parse("tel: 02-222-2222")
-            val myyintent = Intent( Intent.ACTION_CALL, myUri)
-            startActivity(myyintent)
+            //TedPermission 라이브러리 활용 -> 권한 여부 대응 + 실제 체크
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    //권한 승인되었을때 실행할 코드를 적는 부분
+                    //전화 연결 활용
+                    val myUri = Uri.parse("tel: 02-222-2222")
+                    val myintent = Intent( Intent.ACTION_CALL, myUri)
+                    startActivity(myintent)
+
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(mContext, "권한이 거부되어 전화연결이 불가합니다.", Toast.LENGTH_SHORT).show()
+
+
+                }
+
+
+            }
+
+
         }
 
         profileImg.setOnClickListener {
