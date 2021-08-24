0ee32a8e240281e0da3935c40a5ec5faaf367ee9
김현지 <guswlhjk@naver.com>
2021-08-25 00:59:02 +0900

    사진 크게보기 화면 진입 - 해당 화면 액션바 제거

--- app/src/main/java/com/nepplus/librarypractice_20210824/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20210824/MainActivity.kt
@@ -1,28 +1,44 @@
 package com.nepplus.librarypractice_20210824
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
 
+    override fun setupEvents() {
+
+        profileImg.setOnClickListener {
+
+            //프로필 사진 클릭 이벤트 => 사진 크게 보기로 이동
+            val myIntent = Intent(mContext, ViewPhotoActivity :: class.java)
+            startActivity(myIntent)
 
 
-        override fun setupEvents() {
 
-
+        }
 
     }
 
-        override fun setValues() {
-
-
+    override fun setValues() {
 
     }
+
+
+
+
+
+
+
+
 
 
     }
---
+++ app/src/main/java/com/nepplus/librarypractice_20210824/ViewPhotoActivity.kt
@@ -0,0 +1,29 @@
+package com.nepplus.librarypractice_20210824
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+        setupEvents()
+        setValues()
+
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+
+
+
+    }
+}
